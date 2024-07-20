0c25cb0de550edc57d9cd418b85f74908462bfbf
Andrii <semenukandrej574@gmailcom>
2024-07-20 23:56:37 +0200

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,20 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        Customer customer = getCustomer(getData());
+        String output = "Сustomer: " + customer.getName() + ", phone: " + customer.getPhone(); ;
+        getOutput(output);
+
+    }
+    public static String[] getData() {
+        return new String []{"Tom", "555 123-8596"};
+    }
+    public static Customer getCustomer(String[] data) {
+        return new Customer(data[0], data[1]);
+    }
+    public static void getOutput(String output) {
+        System.out.println(output);
+    }
+}
+
